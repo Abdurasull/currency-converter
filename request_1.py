3b335820475976cf1d9edd8b54bc657da7d1c538
Abdurasull <abdurasulsalimov94@gmail.com>
2025-01-12 11:58:22 +0500

    bajarildi

--- request_1.py
+++ request_1.py
@@ -23,10 +23,12 @@ def menu() -> None :
 
 
 # natijani fayl ga qo`shuvchi function
-def add_result(valyuta: float) ->None:
+def add_result(valyuta) ->None:
     
+    hozirgi_vaqt = datetime.now()
+
     # vaqtni aniqlab olish
-    time_result = datetime.strftime("%Y-%m-%d  %H:%M:%S")
+    time_result = hozirgi_vaqt.strftime("%Y-%m-%d  %H:%M:%S")
     
     with open("valyuta.txt", "a") as file:
         file.write(f"{time_result} da yaratildi: {str(valyuta)}\n\n")
@@ -34,69 +36,82 @@ def add_result(valyuta: float) ->None:
 
 # asosiy function ma`lumotlarni shu erdan izlanadi va shu erda natija e`lon qilinadi
 def return_result() -> int: 
-    # konvertatsiya qilmoqchi bo`lgan summa
-    summa = float(input("Summani kiriting: "))
-    if summa != 0:    
-        # Qaysi valyutadan konvertatsiya qilmoqchisiz
-        valyuta1 = input('Qaysi valyutadan konvertatsiya qilmoqchisiz (masalan, USD):')
-        exitt(valyuta1)
+    while True:
+        try:
+            # konvertatsiya qilmoqchi bo`lgan summa
+            summa = float(input("Summani kiriting: "))
+            if summa != 0:    
+                # Qaysi valyutadan konvertatsiya qilmoqchisiz
+                valyuta1 = input('Qaysi valyutadan konvertatsiya qilmoqchisiz (masalan, USD):')
+                exitt(valyuta1)
+                if valyuta1 == '0':
+                    print(f"{Fore.RED}Buncha tiz ketyabsiz??? {Fore.RESET}🥸")
+                    exit()
 
-        # Qaysi valyutaga konvertatsiya qilmoqchisiz
-        valyuta2 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
-        exitt(valyuta2)
+                # Qaysi valyutaga konvertatsiya qilmoqchisiz
+                valyuta2 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
+                exitt(valyuta2)
+                if valyuta2 == '0':
+                    print(f"{Fore.RED}Buncha tiz ketyabsiz??? {Fore.RESET}🥸")
+                    exit()
 
-        # requests kutubxonasi yordamida url dagi ma`lumotni o`zgaruvchiga ta`minlab olamiz
-        requests_sums = requests.get(url.URL)
+                # requests kutubxonasi yordamida url dagi ma`lumotni o`zgaruvchiga ta`minlab olamiz
+                requests_sums = requests.get(url.URL)
 
-        # url dan olgan ma`lumotimmiz string turida bo`lgani uchun uni json() function yordamida json ko`rinishi ga o`girib olamiz
-        requests_list = requests_sums.json()
-        
-        # valyuta tug`ri kiritilganligini tikshirish un
-        count_check = 0
+                # url dan olgan ma`lumotimmiz string turida bo`lgani uchun uni json() function yordamida json ko`rinishi ga o`girib olamiz
+                requests_list = requests_sums.json()
+                
+                # valyuta tug`ri kiritilganligini tikshirish un
+                count_check = 0
 
-        # konvertatsiya qilmoqchi bo`lgan valyutalarni for loop yordamida ularni tan narxini chiqaramiz izlaymiz
-        if valyuta1.upper() == 'UZS':
-            while True:
-                for valyuta in requests_list: 
-                    if valyuta2.upper() == valyuta["code"]:
-                        result = f"Natija: {summa} UZS = {round(summa / float(valyuta['cb_price']), 2)} {valyuta['code']}"
-                        add_result(result)
-                        print(f"\n{Fore.LIGHTCYAN_EX}Natija: {summa} UZS = {round(summa / float(valyuta['cb_price']), 2)} {valyuta['code']}{Fore.RESET}")
-                        count_check +=1
-                        break
-                # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
-                if count_check == 0:
-                    print(f"\n{Fore.RED}Siz kiritgan ->{Fore.YELLOW} '{valyuta2}' {Fore.RESET}{Fore.RED}valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
-                    valyuta2 = input(f'Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
-                    exitt(valyuta2)
+                # konvertatsiya qilmoqchi bo`lgan valyutalarni for loop yordamida ularni tan narxini chiqaramiz izlaymiz
+                if valyuta1.upper() == 'UZS':
+                    while True:
+                        for valyuta in requests_list: 
+                            if valyuta2.upper() == valyuta["code"]:
+                                result = f"Natija: {summa} UZS = {round(summa / float(valyuta['cb_price']), 2)} {valyuta['code']}"
+                                add_result(result)
+                                print(f"\n{Fore.LIGHTCYAN_EX}Natija: {summa} UZS = {round(summa / float(valyuta['cb_price']), 2)} {valyuta['code']}{Fore.RESET}")
+                                count_check +=1
+                                break
+                        # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
+                        if count_check == 0:
+                            print(f"\n{Fore.RED}Siz kiritgan ->{Fore.YELLOW} '{valyuta2}' {Fore.RESET}{Fore.RED}valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
+                            valyuta2 = input(f'Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
+                            exitt(valyuta2)
+                        else:
+                            break
                 else:
-                    break
-        else:
-            count_check = 0
-            while True:
-                for valyuta in requests_list:
-                    if valyuta1.upper() == valyuta['code']:
-                        while True:
-                            for val in requests_list:
-                                if valyuta2.upper() == val['code']:
-                                    count_check +=1
-                                    result = f"\nNatija: {summa} {valyuta['code']} = {round(float(valyuta['cb_price']) / float(val['cb_price'])*summa, 2)} {val['code']}"
-                                    add_result(result)
-                                    print(f"\n{Fore.LIGHTCYAN_EX}Natija: {summa} {valyuta['code']} = {round(float(valyuta['cb_price']) / float(val['cb_price'])*summa, 2)} {val['code']}{Fore.RESET}")        
-                            # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
-                            if count_check == 0:
-                                print(f"\n{Fore.RED}Siz kiritgan -> {Fore.YELLOW}'{valyuta2}'{Fore.RED} valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
-                                valyuta2 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
-                                exitt(valyuta2)
-                            else:
-                                break  
-                # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
-                if count_check == 0:
-                    print(f"\n{Fore.RED}Siz kiritgan -> {Fore.YELLOW}'{valyuta1}' {Fore.RED}valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
-                    valyuta1 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
-                    exitt(valyuta1)
-                else:
-                    break  
-    else:
-        print(f"{Fore.RED}Buncha tiz ketyabsiz??? {Fore.RESET}🥸")
-
+                    count_check = 0
+                    while True:
+                        for valyuta in requests_list:
+                            if valyuta1.upper() == valyuta['code']:
+                                while True:
+                                    for val in requests_list:
+                                        if valyuta2.upper() == val['code']:
+                                            count_check +=1
+                                            result = f"\nNatija: {summa} {valyuta['code']} = {round(float(valyuta['cb_price']) / float(val['cb_price'])*summa, 2)} {val['code']}"
+                                            add_result(result)
+                                            print(f"\n{Fore.LIGHTCYAN_EX}Natija: {summa} {valyuta['code']} = {round(float(valyuta['cb_price']) / float(val['cb_price'])*summa, 2)} {val['code']}{Fore.RESET}")        
+                                    # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
+                                    if count_check == 0:
+                                        print(f"\n{Fore.RED}Siz kiritgan -> {Fore.YELLOW}'{valyuta2}'{Fore.RED} valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
+                                        valyuta2 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
+                                        exitt(valyuta2)
+                                    else:
+                                        break  
+                        # agar count_check 0 ga teng bo`lsa demak biz kiritgan valyuta xato
+                        if count_check == 0:
+                            print(f"\n{Fore.RED}Siz kiritgan -> {Fore.YELLOW}'{valyuta1}' {Fore.RED}valyuta mavjud emas, iltimos tikshirib qaytadan kiriting!{Fore.RESET}\n")
+                            valyuta1 = input('Qaysi valyutaga konvertatsiya qilmoqchisiz (masalan, UZS):')
+                            exitt(valyuta1)
+                        else:
+                            break  
+            
+            else:
+                print(f"{Fore.RED}Buncha tiz ketyabsiz??? {Fore.RESET}🥸")
+                exit()
+        except ValueError:
+                        
+            print(f"{Fore.RED}Iltimos, faqat butun yoki float son kiriting!{Fore.RESET}\n")
+            
